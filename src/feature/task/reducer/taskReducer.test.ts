d3a9dfa237d409eb88ac25005f8b23cde1254fb3
Ramon Trepat <r.trepis@gmail.com>
2022-08-09 22:43:02 +0200

    Add test actions resource

--- src/feature/task/reducer/taskReducer.test.ts
+++ src/feature/task/reducer/taskReducer.test.ts
@@ -1,36 +1,26 @@
 import ITask from "../models/ITask";
 import taskReducer from "./taskReducer";
 import {
+  createTaskActionCreator,
   deleteTaskActionCreator,
   loadTaskActionCreator,
+  updateTaskActionCreator,
 } from "./actionCreators";
 
 describe("Give a reducer", () => {
+  const previeousTasks: ITask[] = [];
   describe("When a have inicial state", () => {
     test("Then a instanciated with a unknow action", () => {
       const unknownActionTest = { type: "nose" };
-      const previewState: ITask[] = [
-        {
-          id: 1,
-          name: "Abrir las devtools",
-          isDone: false,
-        },
-        {
-          id: 2,
-          name: "Tarea 2",
-          isDone: false,
-        },
-      ];
 
-      const expectedListTasks = taskReducer(previewState, unknownActionTest);
+      const expectedListTasks = taskReducer(previeousTasks, unknownActionTest);
 
-      expect(expectedListTasks).toStrictEqual(previewState);
+      expect(expectedListTasks).toStrictEqual(previeousTasks);
     });
   });
 
   describe("When instanciate load action with new state", () => {
     test("Then new state to be a new task", () => {
-      const previeousTasks: ITask[] = [];
       const newTask: ITask[] = [
         { id: 1, name: "Abrir las deptools", isDone: false },
       ];
@@ -39,6 +29,21 @@ describe("Give a reducer", () => {
       const newState = taskReducer(previeousTasks, action);
 
       expect(newState).toStrictEqual(newTask);
+    });
+  });
+
+  describe("When instanciate add action with task id", () => {
+    test("Then new state to be new task", () => {
+      const addTask: ITask = {
+        id: 1,
+        name: "Abrir las deptools",
+        isDone: false,
+      };
+      const action = createTaskActionCreator(addTask);
+
+      const newState = taskReducer(previeousTasks, action);
+
+      expect(newState.length).toBe(previeousTasks.length + 1);
     });
   });
 
@@ -61,6 +66,28 @@ describe("Give a reducer", () => {
       const newState = taskReducer(previeousTask, action);
 
       expect(newState.length).toBe(previeousTask.length - 1);
+    });
+  });
+
+  describe("When instanciate update action with modify task", () => {
+    test("Then new state to be this modify task", () => {
+      const previeousTask: ITask[] = [
+        {
+          id: 1,
+          name: "Abrir las devtools",
+          isDone: false,
+        },
+      ];
+      const modifyTask: Partial<ITask> = {
+        id: 1,
+        isDone: true,
+      };
+
+      const action = updateTaskActionCreator(modifyTask);
+
+      const newState = taskReducer(previeousTask, action);
+
+      expect(newState).not.toEqual(previeousTask);
     });
   });
 });
